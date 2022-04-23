07e23c53e0073402304d7e5be2dd08e2aeeb5529
DiegoPassa <diegopassarella01@gmail.com>
2022-04-23 19:27:20 +0200

    test commit + templates

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
 from lib.conn import get_engine
 app = Flask(__name__)
-
 
 
 @app.route('/')
 def index():
     engine = get_engine()
     return engine.url.database
+
+test = 'Hello world'
+
+@app.route('/login')
+def login():
+    return render_template('login.html', title=test)
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html', title=test)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
