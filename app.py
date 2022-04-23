057bd63cbbe9baabe89aea539743703c0d3c0256
DiegoPassa <diegopassarella01@gmail.com>
2022-04-23 22:53:53 +0200

    partially added flask login

--- app.py
+++ app.py
@@ -1,18 +1,83 @@
-from flask import Flask, render_template
+from cgitb import reset
+from crypt import methods
+from flask import Flask, make_response, redirect, render_template, request, url_for
+# import di flask login
+from flask_login import *
+# import from lib/conn -> connessione al database
 from lib.conn import get_engine
 app = Flask(__name__)
+
+# inizializza flask_login
+app.config['secret_key'] = 'passwordsupersegreta'
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+class User(UserMixin):
+    # costruttore di classe
+    def __init__(self, id, email, pwd):
+        self.id = id
+        self.email = email
+        self.pwd = pwd
+
+
+def get_user_by_email(email):
+    conn = engine.connect()
+    rs = conn.execute('SELECT * FROM users WHERE email = ?', email)
+    user = rs.fetchone()
+    conn.close()
+    return User(user.id, user.email, user.pwd)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    conn = engine.connect()
+    rs = conn.execute('SELECT * FROM users WHERE id_user = ?', user_id)
+    user = rs.fetchone()
+    conn.close()
+    return User(user.id, user.mail, user.pwd)
 
 
 @app.route('/')
 def index():
-    engine = get_engine()
-    return engine.url.database
+    if current_user.is_authenticated:
+        return redirect(url_for('private'))
+    return render_template('index.html')
+
 
 test = 'Hello world'
 
-@app.route('/login')
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html', title=test)
+    if request.method == 'POST':
+        conn = engine.connect()
+        rs = conn.execute('SELECT pwd FROM students WHERE email = ?', [
+                          request.form['user']])
+        real_pwd = rs.fetchone()
+        conn.close()
+
+        if (real_pwd is not None):
+            # resto della logica di autenticazione
+            pass
+        else:
+            return redirect(url_for('index'))
+    else:
+        return redirect(url_for('index'))
+
+    if request.form['pass'] == real_pwd['pwd']:
+        user = get_user_by_email(request.form['user'])
+        login_user(user)
+        return redirect(url_for('private'))
+    else:
+        return redirect(url_for('index'))
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 
 @app.route('/register')
@@ -20,5 +85,18 @@ def register():
     return render_template('register.html', title=test)
 
 
+@app.route('/private')
+@login_required
+def private():
+    conn = engine.connect()
+    users = conn.execute('SELECT * FROM users')
+    resp = make_response(render_template('private.html', user=users))
+    conn.close()
+    return resp
+
+
 if __name__ == '__main__':
+    # inizializza engine db
+    engine = get_engine()
+    # fai partire l'applicazione Flask
     app.run(debug=True)
